50883a6e6644d78bcffc4314204f8ce2cb54294c
Dastan Kudaibergen <dastan.kudaibergen@sharp-dev.net>
2024-07-06 19:12:45 +0500

    changed baseApi

--- ClientApp/src/redux/api/index.ts
+++ ClientApp/src/redux/api/index.ts
@@ -2,9 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  endpoints: (builder) => ({}),
+  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  endpoints: () => ({}),
 });
 
 export default baseApi;
